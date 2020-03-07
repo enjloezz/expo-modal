c62d0d74d33e498bc4b244e301267cffbc4cc899
Hakkı Dizdar <hakkidizdar@mail.com.tr>
2020-03-08 00:33:06 +0300

    merged with react-native-scl-alert

--- GenericModal.js
+++ GenericModal.js
@@ -1,57 +1,109 @@
-import React, { Component } from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import PropTypes from "prop-types";
+import { SCLAlert, SCLAlertButton } from "react-native-scl-alert";
 
-const { width, height } = Dimensions.get('window');
-
+const { width, height } = Dimensions.get("window");
 
 class GenericModal extends Component {
+  static propTypes = {
+    containerStyle: PropTypes.object
+  };
+  state = {
+    isVisible: false,
+    title: null,
+    subtitle: null,
+    buttons: null,
+    theme: "default"
+  };
 
-    static propTypes = {
-      containerStyle: PropTypes.object,
-    };
-    state = {
-        isVisible: false,
-        component: null,
-    };
+  dismissModal = () => {
+    this.setState({
+      isVisible: false
+    });
+  };
 
-    dismissModal = () => {
-        this.setState({
-            isVisible: false,
-        });
-    };
+  showModal = (
+    title: string,
+    subtitle: string,
+    theme: string,
+    buttons: array
+  ) => {
+    this.setState({
+      isVisible: true,
+      title,
+      subtitle,
+      buttons,
+      theme
+    });
+  };
 
-    showModal = (component) => {
-        this.setState({
-            isVisible: true,
-            component,
-        });
-    };
+  onClose = () => {
+    this.setState({
+      title: null,
+      subtitle: null,
+      buttons: null,
+      theme: "default"
+    });
+  };
 
-    render() {
-        const { isVisible, component } = this.state;
-        const {containerStyle} = this.props;
-        if (!isVisible) return null;
+  render() {
+    const { isVisible } = this.state;
+    const { containerStyle } = this.props;
+    if (!isVisible) return null;
 
-        return (
-            <View style={[styles.container, containerStyle]}>
-                {component}
-            </View>);
-    }
+    return (
+      <View style={[styles.container, containerStyle]}>
+        <SCLAlert
+          theme={this.state.theme}
+          show={this.state.isVisible}
+          title={this.state.title}
+          subtitle={this.state.subtitle}
+          onRequestClose={this.onClose}
+        >
+          {this.state.buttons === null ? (
+            <SCLAlertButton theme="default" onPress={this.dismissModal}>
+              Tamam
+            </SCLAlertButton>
+          ) : (
+            this.state.buttons !== undefined &&
+            this.state.buttons.map((item, i) => {
+              return (
+                <SCLAlertButton
+                  key={"alert-button-" + i}
+                  theme={item.theme !== undefined ? item.theme : "default"}
+                  onPress={
+                    item.style === "cancel"
+                      ? () => {
+                          item.onPress && item.onPress();
+                          this.dismissModal();
+                        }
+                      : item.onPress
+                  }
+                >
+                  {item.text}
+                </SCLAlertButton>
+              );
+            })
+          )}
+        </SCLAlert>
+      </View>
+    );
+  }
 }
 
 export default GenericModal;
 
 const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        height,
-        width,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'rgba(255,255,255,0.5)',
-        zIndex: 100,
-    },
+  container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height,
+    width,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(255,255,255,0.5)",
+    zIndex: 100
+  }
 });
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 import GenericModal from "./GenericModal";
 import Util from "./Util";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 class Modal extends React.Component {
+  static propTypes = {
+    containerStyle: PropTypes.object
+  };
 
-    static propTypes = {
-        containerStyle: PropTypes.object
-    };
-
-    render() {
-        return (
-            <GenericModal containerStyle={this.props.containerStyle} ref={ref=> Util.setModalRef(ref)} />
-        )
-    }
+  render() {
+    return (
+      <GenericModal
+        containerStyle={this.props.containerStyle}
+        ref={ref => Util.setModalRef(ref)}
+      />
+    );
+  }
 }
 
-exports default {
-    Modal,
-    showModal: Util.showModal,
-    dismissModal: Util.dismissModal,
-    wrapIntoModal: Util.wrapIntoModal,
+module.exports = {
+  Modal,
+  showModal: Util.showModal,
+  dismissModal: Util.dismissModal,
+  wrapIntoModal: Util.wrapIntoModal
 };
